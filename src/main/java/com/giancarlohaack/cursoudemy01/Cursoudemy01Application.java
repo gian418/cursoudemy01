ade2269c472ae1e396b355ffe4e42cf4ac09bca5
Giancarlo Haack Albano <gian@jlinformatica.net.br>
2018-05-21 16:45:32 -0300

    Section 2, Lecture 16

--- src/main/java/com/giancarlohaack/cursoudemy01/Cursoudemy01Application.java
+++ src/main/java/com/giancarlohaack/cursoudemy01/Cursoudemy01Application.java
@@ -1,12 +1,15 @@
 package com.giancarlohaack.cursoudemy01;
 
 import com.giancarlohaack.cursoudemy01.domain.Categoria;
+import com.giancarlohaack.cursoudemy01.domain.Produto;
 import com.giancarlohaack.cursoudemy01.repositories.CategoriaRepository;
+import com.giancarlohaack.cursoudemy01.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 @SpringBootApplication
@@ -14,6 +17,9 @@ public class Cursoudemy01Application implements CommandLineRunner {
 
     @Autowired
     private CategoriaRepository categoriaRepository;
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(Cursoudemy01Application.class, args);
@@ -24,6 +30,18 @@ public class Cursoudemy01Application implements CommandLineRunner {
         Categoria categoria1 = new Categoria(null, "Informática");
         Categoria categoria2 = new Categoria(null, "Escritório");
 
+        Produto produto1 = new Produto(null, "Computador", 2000D);
+        Produto produto2 = new Produto(null, "Impressora", 800D);
+        Produto produto3 = new Produto(null, "Mouse", 80D);
+
+        categoria1.getProdutos().addAll(Arrays.asList(produto1, produto2, produto3));
+        categoria2.getProdutos().addAll(Arrays.asList(produto2));
+
+        produto1.getCategorias().addAll(Arrays.asList(categoria1));
+        produto2.getCategorias().addAll(Arrays.asList(categoria1, categoria2));
+        produto3.getCategorias().addAll(Arrays.asList(categoria1));
+
         categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
+        produtoRepository.saveAll(Arrays.asList(produto1, produto2, produto3));
     }
 }
