bb06b5a06976c6bf6f8653b604c79de94c6a9d6e
Giancarlo Haack Albano <gian@jlinformatica.net.br>
2018-05-21 15:45:49 -0300

    Section 2, Lecture 15

--- src/main/java/com/giancarlohaack/cursoudemy01/Cursoudemy01Application.java
+++ src/main/java/com/giancarlohaack/cursoudemy01/Cursoudemy01Application.java
@@ -1,12 +1,29 @@
 package com.giancarlohaack.cursoudemy01;
 
+import com.giancarlohaack.cursoudemy01.domain.Categoria;
+import com.giancarlohaack.cursoudemy01.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class Cursoudemy01Application {
+public class Cursoudemy01Application implements CommandLineRunner {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(Cursoudemy01Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Categoria categoria1 = new Categoria(null, "Informática");
+        Categoria categoria2 = new Categoria(null, "Escritório");
+
+        categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
     }
 }
