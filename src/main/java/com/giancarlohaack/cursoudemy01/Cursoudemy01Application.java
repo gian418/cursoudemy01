bd38969bac29c985e4f984427cd97d9c4a7a4f8a
Giancarlo Haack Albano <gian@jlinformatica.net.br>
2018-05-21 11:40:04 -0300

    Initial commit

---
+++ src/main/java/com/giancarlohaack/cursoudemy01/Cursoudemy01Application.java
@@ -0,0 +1,12 @@
+package com.giancarlohaack.cursoudemy01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cursoudemy01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cursoudemy01Application.class, args);
+    }
+}
