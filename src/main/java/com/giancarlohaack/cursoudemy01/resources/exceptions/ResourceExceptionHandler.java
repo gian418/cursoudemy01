6d4d51f0fa270e929e1c5abcbb7c6d89c64659dd
Giancarlo Haack Albano <gian418@gmail.com>
2018-05-22 19:39:32 -0300

    Section 2, Lecture 33

--- src/main/java/com/giancarlohaack/cursoudemy01/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/giancarlohaack/cursoudemy01/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.giancarlohaack.cursoudemy01.resources.exceptions;
 
+import com.giancarlohaack.cursoudemy01.services.exceptions.DataIntegrityException;
 import com.giancarlohaack.cursoudemy01.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +21,17 @@ public class ResourceExceptionHandler {
         );
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+    }
+
+    @ExceptionHandler(DataIntegrityException.class)
+    public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
+        StandardError err = new StandardError(
+                HttpStatus.BAD_REQUEST.value(),
+                e.getMessage(),
+                System.currentTimeMillis()
+        );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 
 }
--- src/main/java/com/giancarlohaack/cursoudemy01/services/CategoriaService.java
+++ src/main/java/com/giancarlohaack/cursoudemy01/services/CategoriaService.java
@@ -2,8 +2,10 @@ package com.giancarlohaack.cursoudemy01.services;
 
 import com.giancarlohaack.cursoudemy01.domain.Categoria;
 import com.giancarlohaack.cursoudemy01.repositories.CategoriaRepository;
+import com.giancarlohaack.cursoudemy01.services.exceptions.DataIntegrityException;
 import com.giancarlohaack.cursoudemy01.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import javax.swing.text.html.Option;
@@ -31,6 +33,15 @@ public class CategoriaService {
     public Categoria update(Categoria obj) {
         find(obj.getId()); //Verificar se existe, se nao lança excecao
         return categoriaRepository.save(obj);
+    }
+
+    public void delete(Integer id) {
+        find(id);
+        try {
+            categoriaRepository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+        }
     }
 
 }
