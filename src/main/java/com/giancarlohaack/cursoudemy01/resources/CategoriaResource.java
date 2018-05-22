f758cf0e849c09e5c5eb380dbabad331dea4eab1
Giancarlo Haack Albano <gian418@gmail.com>
2018-05-22 20:10:51 -0300

    Section 3, Lecture 34

--- src/main/java/com/giancarlohaack/cursoudemy01/resources/CategoriaResource.java
+++ src/main/java/com/giancarlohaack/cursoudemy01/resources/CategoriaResource.java
@@ -1,6 +1,7 @@
 package com.giancarlohaack.cursoudemy01.resources;
 
 import com.giancarlohaack.cursoudemy01.domain.Categoria;
+import com.giancarlohaack.cursoudemy01.dto.CategoriaDTO;
 import com.giancarlohaack.cursoudemy01.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value="/categorias")
@@ -46,6 +48,13 @@ public class CategoriaResource {
     public ResponseEntity delete(@PathVariable Integer id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity findAll() {
+        List<Categoria> list = service.findAll();
+        List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
     }
 
 }
--- src/main/java/com/giancarlohaack/cursoudemy01/services/CategoriaService.java
+++ src/main/java/com/giancarlohaack/cursoudemy01/services/CategoriaService.java
@@ -9,6 +9,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import javax.swing.text.html.Option;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -42,6 +43,10 @@ public class CategoriaService {
         } catch (DataIntegrityViolationException e) {
             throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
         }
+    }
+
+    public List<Categoria> findAll() {
+        return categoriaRepository.findAll();
     }
 
 }
