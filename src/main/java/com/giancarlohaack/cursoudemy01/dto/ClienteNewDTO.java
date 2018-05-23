171d993e0c10d063cb5181a80ce2b4102b472179
Giancarlo Haack Albano <gian@jlinformatica.net.br>
2018-05-23 17:55:29 -0300

    Section 3, Lecture 40 parcial

---
+++ src/main/java/com/giancarlohaack/cursoudemy01/dto/ClienteNewDTO.java
@@ -0,0 +1,20 @@
+package com.giancarlohaack.cursoudemy01.dto;
+
+public class ClienteNewDTO {
+
+    private String nome;
+    private String email;
+    private String cpfOuCnpj;
+    private Integer tipo;
+
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cep;
+
+    private String telefone1;
+    private String telefone2;
+    private String telefone3;
+
+}
