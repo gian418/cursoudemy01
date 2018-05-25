e3e05e71c0f804a86999c7879b40fde49541fedf
Giancarlo Haack Albano <gian@jlinformatica.net.br>
2018-05-25 11:10:13 -0300

    Section 3, Lecture 42

---
+++ src/main/java/com/giancarlohaack/cursoudemy01/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,37 @@
+package com.giancarlohaack.cursoudemy01.services.validation;
+
+import com.giancarlohaack.cursoudemy01.domain.enums.TipoCliente;
+import com.giancarlohaack.cursoudemy01.dto.ClienteNewDTO;
+import com.giancarlohaack.cursoudemy01.resources.exceptions.FieldMessage;
+import com.giancarlohaack.cursoudemy01.services.validation.utils.BR;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+    @Override
+    public void initialize(ClienteInsert ann) {
+    }
+
+    @Override
+    public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+        List<FieldMessage> list = new ArrayList<>();
+
+        if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+            list.add(new FieldMessage("cpfOuCpnj", "CPF inválido"));
+        }
+
+        if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+            list.add(new FieldMessage("cpfOuCpnj", "CNPJ inválido"));
+        }
+
+        for (FieldMessage e : list) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(e.getMessage())
+                    .addPropertyNode(e.getFieldName()).addConstraintViolation();
+        }
+        return list.isEmpty();
+    }
+}
