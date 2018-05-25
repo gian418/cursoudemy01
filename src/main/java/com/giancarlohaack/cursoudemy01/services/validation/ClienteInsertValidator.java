d046747d633458a4147f4c3557b45a71265f556b
Giancarlo Haack Albano <gian@jlinformatica.net.br>
2018-05-25 14:26:35 -0300

    Section 3, Lecture 44

--- src/main/java/com/giancarlohaack/cursoudemy01/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/giancarlohaack/cursoudemy01/services/validation/ClienteInsertValidator.java
@@ -1,9 +1,13 @@
 package com.giancarlohaack.cursoudemy01.services.validation;
 
+import ch.qos.logback.core.net.server.Client;
+import com.giancarlohaack.cursoudemy01.domain.Cliente;
 import com.giancarlohaack.cursoudemy01.domain.enums.TipoCliente;
 import com.giancarlohaack.cursoudemy01.dto.ClienteNewDTO;
+import com.giancarlohaack.cursoudemy01.repositories.ClienteRepository;
 import com.giancarlohaack.cursoudemy01.resources.exceptions.FieldMessage;
 import com.giancarlohaack.cursoudemy01.services.validation.utils.BR;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +15,10 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+    @Autowired
+    private ClienteRepository repo;
+
     @Override
     public void initialize(ClienteInsert ann) {
     }
@@ -25,6 +33,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 
         if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
             list.add(new FieldMessage("cpfOuCpnj", "CNPJ inválido"));
+        }
+
+        Cliente aux = repo.findByEmail(objDto.getEmail());
+        if (aux != null) {
+            list.add(new FieldMessage("email", "Email já existente"));
         }
 
         for (FieldMessage e : list) {
