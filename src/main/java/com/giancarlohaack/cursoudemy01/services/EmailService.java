f61dc110e645db577f5a77c7b685de9cbe055a4a
Giancarlo Haack Albano <gian418@gmail.com>
2018-05-28 19:57:03 -0300

    secao 5 aula 63

--- src/main/java/com/giancarlohaack/cursoudemy01/services/EmailService.java
+++ src/main/java/com/giancarlohaack/cursoudemy01/services/EmailService.java
@@ -1,11 +1,17 @@
 package com.giancarlohaack.cursoudemy01.services;
 
 import com.giancarlohaack.cursoudemy01.domain.Pedido;
+
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 public interface EmailService {
 
     void sendOrderConfirmationEmail(Pedido obj);
     void sendEmail(SimpleMailMessage msg);
+    
+    void sendOrderConfirmationHtmlEmail(Pedido obj);
+    void sendHtmlEmail(MimeMessage msg);
 
 }
