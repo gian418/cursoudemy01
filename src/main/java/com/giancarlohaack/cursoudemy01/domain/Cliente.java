3c4d1086adb69e9dd912bbbde620d81aa43be8ec
Giancarlo Haack Albano <gian@jlinformatica.net.br>
2018-05-29 13:41:56 -0300

    secao 6 aula 67 Salvando perfis de usuario

--- src/main/java/com/giancarlohaack/cursoudemy01/domain/Cliente.java
+++ src/main/java/com/giancarlohaack/cursoudemy01/domain/Cliente.java
@@ -1,11 +1,13 @@
 package com.giancarlohaack.cursoudemy01.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.giancarlohaack.cursoudemy01.domain.enums.Perfil;
 import com.giancarlohaack.cursoudemy01.domain.enums.TipoCliente;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Entity
 public class Cliente implements Serializable {
@@ -31,11 +33,17 @@ public class Cliente implements Serializable {
     @CollectionTable(name = "TELEFONE")
     private Set<String> telefones = new HashSet<>();
 
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "PERFIS")
+    private Set<Integer> perfis = new HashSet<>();
+
+
     @JsonIgnore
     @OneToMany(mappedBy = "cliente")
     private List<Pedido> pedidos = new ArrayList<>();
 
     public Cliente() {
+        addPerfil(Perfil.CLIENTE);
     }
 
     public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipo, String senha) {
@@ -45,6 +53,7 @@ public class Cliente implements Serializable {
         this.cpfOuCnpj = cpfOuCnpj;
         this.tipo = (tipo == null) ? null : tipo.getCod();
         this.senha = senha;
+        addPerfil(Perfil.CLIENTE);
     }
 
     public Integer getId() {
@@ -117,6 +126,14 @@ public class Cliente implements Serializable {
 
     public void setSenha(String senha) {
         this.senha = senha;
+    }
+
+    public Set<Perfil> getPerfis() {
+        return perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
+    }
+
+    public void addPerfil(Perfil perfil) {
+        perfis.add(perfil.getCod());
     }
 
     @Override
--- src/main/java/com/giancarlohaack/cursoudemy01/services/DBService.java
+++ src/main/java/com/giancarlohaack/cursoudemy01/services/DBService.java
@@ -2,6 +2,7 @@ package com.giancarlohaack.cursoudemy01.services;
 
 import com.giancarlohaack.cursoudemy01.domain.*;
 import com.giancarlohaack.cursoudemy01.domain.enums.EstadoPagamento;
+import com.giancarlohaack.cursoudemy01.domain.enums.Perfil;
 import com.giancarlohaack.cursoudemy01.domain.enums.TipoCliente;
 import com.giancarlohaack.cursoudemy01.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -99,13 +100,19 @@ public class DBService {
         Cliente cli1 = new Cliente(null, "Maria Silva", "gian418@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, pe.encode("123"));
         cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 
+        Cliente cli2 = new Cliente(null, "Ana Costa", "gian418@hotmail.com", "316228382740", TipoCliente.PESSOAFISICA, pe.encode("123"));
+        cli2.getTelefones().addAll(Arrays.asList("93883321", "34252625"));
+        cli2.addPerfil(Perfil.ADMIN);
+
         Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
         Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+        Endereco e3 = new Endereco(null, "Avenida Floriano", "2106", null, "Centro", "85880000", cli2, c2);
 
         cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+        cli2.getEnderecos().addAll(Arrays.asList(e3));
 
-        clienteRepository.saveAll(Arrays.asList(cli1));
-        enderecoRepository.saveAll(Arrays.asList(e1, e2));
+        clienteRepository.saveAll(Arrays.asList(cli1, cli2));
+        enderecoRepository.saveAll(Arrays.asList(e1, e2, e3));
 
         //Pedidos
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
