9bf6a4791ffa460371b4bbd9a8f5fb2551f14e8a
Giancarlo Haack Albano <gian@jlinformatica.net.br>
2018-05-30 14:42:11 -0300

    secao 6 aula 70 Implementando autorizacao

--- src/main/java/com/giancarlohaack/cursoudemy01/config/SecurityConfig.java
+++ src/main/java/com/giancarlohaack/cursoudemy01/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.giancarlohaack.cursoudemy01.config;
 
 import com.giancarlohaack.cursoudemy01.security.JWTAthenticationFilter;
+import com.giancarlohaack.cursoudemy01.security.JWTAuthorizationFilter;
 import com.giancarlohaack.cursoudemy01.security.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
@@ -60,6 +61,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated();
 
         http.addFilter(new JWTAthenticationFilter(authenticationManager(), jwtUtil));
+        http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
